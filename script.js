c74ced27d6cf77a15bbf2029b84097e630172769
Ujjwal Bhatt <114344808+Ujjwalb09@users.noreply.github.com>
2023-08-01 22:13:01 +0530

    Refactored the code to follow DRY principles and implemented some minor changes in CSS styles.

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
 'use strict';
 
-/*
-console.log(document.querySelector('.message').textContent);
-
-//manipulating DOM notes of .message element
-document.querySelector('.message').textContent = 'Correct Number!';
-
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
-
 //logic to generate the random number
-
 let randomNumber = Math.trunc(Math.random() * 20) + 1;
 
 document.querySelector('.number').value = randomNumber;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
 console.log(randomNumber);
 let score = 20;
@@ -28,28 +16,23 @@ let highScore = 0;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value); //value that we are taking in the input
 
+  //if input is empty or zero
+  if (!guess) {
+    displayMessage('No Number! Please input a number and try again');
+  }
+
   if (score > 1) {
-    //when guess is greater than number
-    if (guess > randomNumber && guess !== 0) {
-      document.querySelector('.message').textContent = 'Number too large';
-
-      //decreasing the score on every incorrect answer
+    //when the guess is wrong
+    if (guess !== randomNumber && guess !== 0) {
+      displayMessage(
+        guess > randomNumber ? 'Number too large 🙁' : 'Number too small 🙁'
+      );
       score--;
       document.querySelector('.score').textContent = score;
 
-      //when guess is less than number
-    } else if (guess < randomNumber && guess !== 0) {
-      document.querySelector('.message').textContent = 'Number too small';
-
-      //decreasing the score on every incorrect answer
-      score--;
-      document.querySelector('.score').textContent = score;
-
-      //when player wins the game
+      //when the guess is correct
     } else if (guess === randomNumber) {
-      document.querySelector('.message').textContent =
-        'Congratulations, you guessed the right number';
-
+      displayMessage('Congratulations, you guessed the right number 🥇');
       //changing background color to green when player wins the game
       document.querySelector('body').style.backgroundColor = '#60b347';
 
@@ -60,25 +43,22 @@ document.querySelector('.check').addEventListener('click', function () {
       //setting the highscore as current score when guess is correct
       if (score > highScore) {
         highScore = score;
-        document.querySelector('.highscore').textContent = score;
+        document.querySelector('.highscore').textContent = highScore;
       }
     }
   } else {
-    document.querySelector('.message').textContent = 'You lost the game 😭';
+    displayMessage('You lost the game 😭');
 
     document.querySelector('.score').textContent = 0;
-  }
-
-  //if input is empty or zero
-  if (!guess) {
-    document.querySelector('.message').textContent =
-      'No Number! Please input a number and try again';
   }
 });
 
 //applying event listner on again button
 document.querySelector('.again').addEventListener('click', function () {
-  document.querySelector('.number').value = Math.trunc(Math.random() * 20 + 1);
+  score = 20;
+  randomNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.number').value = randomNumber;
 
   randomNumber = document.querySelector('.number').value;
   console.log(randomNumber);
@@ -91,9 +71,7 @@ document.querySelector('.again').addEventListener('click', function () {
 
   document.querySelector('.number').textContent = '?';
 
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing the number 😉');
 
-  document.querySelector('.score').textContent = '20';
-
-  score = 20;
+  document.querySelector('.score').textContent = score;
 });
